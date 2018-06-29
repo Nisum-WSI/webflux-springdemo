fe2a4c78c0daaaa0013a8cb56cd96e0d91ab42b0
WSIServices <40691730+WSIServices@users.noreply.github.com>
2018-06-29 17:06:22 +0530

    Initial commit

---
+++ src/main/java/com/example/webfluxspringdemo/WebfluxSpringdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.webfluxspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxSpringdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/webfluxspringdemo/WebfluxSpringdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webfluxspringdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxSpringdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
